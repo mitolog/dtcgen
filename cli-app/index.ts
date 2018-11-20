1c898c5f7519fde075b3ff5bc6b03c5359c36033
mitolog <hkyhm5l@gmail.com>
2018-11-20 20:25:27 +0700

    added test code to extract semantic data and described procedure of it.

--- cli-app/index.ts
+++ cli-app/index.ts
@@ -1,39 +1,60 @@
-// import Cac from "Cac";
+import Cac from "Cac";
 import { cliContainer } from "../dist/inversify.config";
 import { ILintNamingUseCase } from "../dist/domain/Domain";
 import { TYPES } from "../dist/types";
 
-// todo: config.jsonの読み込み後のnormalizeなり型チェックはeslintのソレを使ってもいいかも
-console.log("now start linting...");
-// prettier-ignore
-const lintNamingUseCase = cliContainer.get<ILintNamingUseCase>(TYPES.ILintNamingUseCase);
-lintNamingUseCase
-  .handle()
-  .then(layers => {
-    console.log("finished linting.");
-    console.log("--------------------");
-    console.log(layers);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const cli = Cac();
 
-// const cli = Cac();
-// cli.command(
-//   "lint",
-//   {
-//     desc: "lint design resource file"
-//   },
-//   (input, flag) => {
-//     // todo: config.jsonの読み込み後のnormalizeなり型チェックはeslintのソレを使ってもいいかも
-//     console.log("now start linting...");
-//     // prettier-ignore
-//     const lintNamingUseCase = cliContainer.get<ILintNamingUseCase>(TYPES.ILintNamingUseCase);
-//     const layers = lintNamingUseCase.handle();
-//     console.log("finished linting.");
-//     console.log("--------------------");
-//     console.log(layers);
-//   }
-// );
+/**
+ * lint
+ */
+cli.command(
+  "lint",
+  {
+    desc: "lint design resource file"
+  },
+  (input, flag) => {
+    // todo: config.jsonの読み込み後のnormalizeなり型チェックはeslintのソレを使ってもいいかも
+    console.log("now start linting...");
+    // prettier-ignore
+    const lintNamingUseCase = cliContainer.get<ILintNamingUseCase>(TYPES.ILintNamingUseCase);
+    lintNamingUseCase
+      .handle()
+      .then(layers => {
+        console.log("finished linting.");
+        console.log("--------------------");
+        console.log(layers);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+);
 
-// cli.parse();
+/**
+ * extract
+ */
+cli.command(
+  "extract",
+  {
+    desc: "extract semantic elements for layout file auto generation."
+  },
+  (input, flag) => {
+    // todo: config.jsonの読み込み後のnormalizeなり型チェックはeslintのソレを使ってもいいかも
+    console.log("now start extracting...");
+    // prettier-ignore
+    const lintNamingUseCase = cliContainer.get<ILintNamingUseCase>(TYPES.ILintNamingUseCase);
+    lintNamingUseCase
+      .handle()
+      .then(layers => {
+        console.log("finished linting.");
+        console.log("--------------------");
+        console.log(layers);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+);
+
+cli.parse();
