43c872e3abb979ceb60ce71284fb82455db98158
mito-san <yuhei.miyazato@innovatube.com>
2019-01-09 15:55:55 +0700

    added template files for Xcode.

---
+++ cli-app/templates/ios/XcodeProjectTemplate/projectName/DesignToCode/ViewConfig.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+protocol ViewConfig {
+    var views: [UIView] { get }
+    var constraints: [Constraint?] { get }
+    func adopt(on onView: UIView)
+    func layoutViews(_ onView: UIView)
+    func configureViews()
+}
