04c6e8257410446fda4ee7a0fce5876f35a526e2
mitolog <hkyhm5l@gmail.com>
2019-03-04 19:57:57 +0700

    modified adopt func to separate dynamic views from static views.

---
+++ templates/ios/XcodeProjectTemplate/projectName/DesignToCode/TreeElement.swift
@@ -0,0 +1,93 @@
+// generated with https://app.quicktype.io/
+// To parse the JSON, add this file to your project and do:
+//
+//   let tree = try Tree(json)
+
+import Foundation
+
+typealias Tree = [TreeElement]
+
+struct TreeElement: Codable {
+    let uid, name: String?
+    let elements: [TreeElement]?
+}
+
+// MARK: Convenience initializers and mutators
+
+extension TreeElement {
+    init(data: Data) throws {
+        self = try newJSONDecoder().decode(TreeElement.self, from: data)
+    }
+
+    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+        guard let data = json.data(using: encoding) else {
+            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+        }
+        try self.init(data: data)
+    }
+
+    init(fromURL url: URL) throws {
+        try self.init(data: try Data(contentsOf: url))
+    }
+
+    func with(
+        uid: String?? = nil,
+        name: String?? = nil,
+        elements: [TreeElement]?? = nil
+        ) -> TreeElement {
+        return TreeElement(
+            uid: uid ?? self.uid,
+            name: name ?? self.name,
+            elements: elements ?? self.elements
+        )
+    }
+
+    func jsonData() throws -> Data {
+        return try newJSONEncoder().encode(self)
+    }
+
+    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
+        return String(data: try self.jsonData(), encoding: encoding)
+    }
+}
+
+extension Array where Element == Tree.Element {
+    init(data: Data) throws {
+        self = try newJSONDecoder().decode(Tree.self, from: data)
+    }
+
+    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+        guard let data = json.data(using: encoding) else {
+            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+        }
+        try self.init(data: data)
+    }
+
+    init(fromURL url: URL) throws {
+        try self.init(data: try Data(contentsOf: url))
+    }
+
+    func jsonData() throws -> Data {
+        return try newJSONEncoder().encode(self)
+    }
+
+    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
+        return String(data: try self.jsonData(), encoding: encoding)
+    }
+}
+
+fileprivate func newJSONDecoder() -> JSONDecoder {
+    let decoder = JSONDecoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    return decoder
+}
+
+fileprivate func newJSONEncoder() -> JSONEncoder {
+    let encoder = JSONEncoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        encoder.dateEncodingStrategy = .iso8601
+    }
+    return encoder
+}
--- templates/ios/XcodeProjectTemplate/projectName/DesignToCode/ViewConfig.swift
+++ templates/ios/XcodeProjectTemplate/projectName/DesignToCode/ViewConfig.swift
@@ -3,8 +3,9 @@ import UIKit
 protocol ViewConfig {
     var views: [String: UIView] { get }
     var constraints: [String: Constraint] { get }
-    func adopt(on onView: UIView)
-    func layoutViews(_ onView: UIView)
+    var treeElement: TreeElement? { get }
+    func adopt(name: String, on onView: UIView)
     func configureViews()
     func getView(_ viewId: String) -> UIView?
+    func getTreeElement(for name: String) -> TreeElement?
 }
